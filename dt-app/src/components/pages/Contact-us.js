e03f167a0778a8c00c2f8b93d76f423a749dacd3
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-11-05 11:09:53 -0600

    Completed layout for contact us, missing content and form to be embedded

--- dt-app/src/components/pages/Contact-us.js
+++ dt-app/src/components/pages/Contact-us.js
@@ -4,7 +4,13 @@ import { Container} from 'react-bootstrap';
 const header = {
     color: 'white',
     fontFamily: 'Bebas Neue, tahoma',
+    letterSpacing: 6,
+}
+
+const blueHeader = {
+    fontFamily: 'Bebas Neue, tahoma',
     letterSpacing: 5,
+    color: '#003054'
 }
 
 const bodyFont = {
@@ -12,23 +18,29 @@ const bodyFont = {
     fontFamily: 'Open Sans, san serif',
 }
 
-const yellowBack = {
+const blueDiv = {
+    backgroundColor:'#003054', 
+    borderRadius:5,
+    boxShadow: '5px 5px 5px #ccc' 
+}
+
+const yellowDiv = {
     backgroundColor: '#e1ad00',
-    borderRadius: 5
+    borderRadius: 5,
 }
 
 const Contact = () => {
 
     return (
         <div style={{paddingTop: 100}}>
-            <div style={{backgroundColor:'#003054'}}>
+            <div style={blueDiv}>
                 <Container>
-                    <Container className='flex-column justify-content-center p-5'>
+                    <Container className='flex-column col-10 justify-content-center pt-5'>
                         <h1 style={header}>Contact Us</h1>
                         <p style={bodyFont}>We know that the tax needs for each person or business are unique. Use our form to tell us more about your needs and concerns. We will give you a free quote for your tax preparation.</p>
                     </Container>
-                    <Container className="p-5 d-flex justify-content-between">
-                        <Container className='p-3' style={yellowBack}>
+                    <Container className="p-5 d-flex justify-content-between row">
+                        <Container className='p-3 mb-5 col-md-5 col-xs-12' style={yellowDiv}>
                             <h3 style={header}>Form</h3>
                             <p style={bodyFont}>Form</p>
                             <p style={bodyFont}>Form</p>
@@ -38,12 +50,12 @@ const Contact = () => {
                             <p style={bodyFont}>Form</p>
                         </Container>
 
-                        <Container className='flex-column align-content-center p-3' style={yellowBack}>
-                            <h3 style={header}>Diversified Tax</h3>
+                        <Container className='flex-column col-md-5 col-xs-12 align-content-center p-3 mb-5' style={yellowDiv}>
+                            <h3 style={blueHeader}>Diversified Tax</h3>
                             <p style={bodyFont}>6532 Miami Lakes Drive,</p>
                             <p style={bodyFont}>Miami Lakes, Florida 33014</p>
                             <br/>
-                            <h3 style={header}>Ricardo Pachon</h3>
+                            <h3 style={blueHeader}>Ricardo Pachon</h3>
                             <p style={bodyFont}>(305)-527-5133</p>
                             <p style={bodyFont}>taxes@diversifiedtax.us</p>
                         </Container>
