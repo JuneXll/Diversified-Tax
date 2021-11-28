ec1fd8e93f377e2f39e91b7260843f33cdef31f8
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-11-28 14:20:08 -0500

    Made navbar mobile friendly with collapse bar

--- dt-app/src/components/Navigation.js
+++ dt-app/src/components/Navigation.js
@@ -18,20 +18,23 @@ const links = {
 const Navigation = () => {
     return (
     
-        <Navbar variant="pills" fixed='top' style={{padding:10, backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
+        <Navbar expand='lg' fixed='top' style={{padding:10, backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
             <Container>
                 <Navbar.Brand href='/' to='/'>
                     <img src={logo} alt="diversified-tax-logo" style={logoStyle}></img>
                 </Navbar.Brand>
-                <Nav.Link href="/personal-tax" style={links}>
-                    Personal Taxes
-                </Nav.Link>
-                <Nav.Link href="/business-tax" style={links}>
-                    Business Taxes
-                </Nav.Link>
-                <Nav.Link href="/contact-us" style={links}>
+                <Navbar.Toggle aria-controls='navbarScroll'/>
+                <Navbar.Collapse className='justify-content-around'>
+                    <Nav.Link href="/personal-tax" style={links}>
+                        Personal Taxes
+                    </Nav.Link>
+                    <Nav.Link href="/business-tax" style={links}>
+                        Business Taxes
+                    </Nav.Link>
+                    <Nav.Link href="/contact-us" style={links}>
                         Contact Us
-                </Nav.Link>
+                    </Nav.Link>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     )
@@ -39,3 +42,22 @@ const Navigation = () => {
 
 export default Navigation;
 
+// return (
+    
+//     <Navbar variant="pills" fixed='top' style={{padding:10, backgroundColor: 'rgba(255, 255, 255, 0.9)'}}>
+//         <Container>
+//             <Navbar.Brand href='/' to='/'>
+//                 <img src={logo} alt="diversified-tax-logo" style={logoStyle}></img>
+//             </Navbar.Brand>
+//             <Nav.Link href="/personal-tax" style={links}>
+//                 Personal Taxes
+//             </Nav.Link>
+//             <Nav.Link href="/business-tax" style={links}>
+//                 Business Taxes
+//             </Nav.Link>
+//             <Nav.Link href="/contact-us" style={links}>
+//                     Contact Us
+//             </Nav.Link>
+//         </Container>
+//     </Navbar>
+// )
