69723ca0c405c7138c171930e973dd5c6c812a68
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-12-03 17:33:14 -0500

    Saving last changes for today

--- src/components/widgets/ContactForm.js
+++ src/components/widgets/ContactForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const blueButton = {
     backgroundColor:'#003054',
@@ -10,27 +10,76 @@ const blueButton = {
 
 
 const ContactForm = () => {
+
+    // const [form, setForm] = useState({});
+    // const [errors, setErrors] = useState({});
+
+    // const setField = (field, value) => {
+    //     setForm({
+    //         ...form,
+    //         [field]:value
+    //     })
+    // }
+
+    // const findFormError = () => {
+    //     const {name, email, phoneNumber, message} = form;
+    //     const newError = {};
+    //     //name errors
+    //     if( !name || name === ''){
+    //         newErrors.name = 'Field cannot be blank.'
+    //     } else if (name.length > 30){
+    //         newErrors.name = "Name is too long."
+    //     }
+    //     //email errors
+        
+    // }
+
     return(
         <form name="contact v1" method="post" className='mt-2'>
             <input type="hidden" name="form-name" value="contact" />
       
             <div className="form-group">
-                <input type="text" className="form-control" id="name" name='name' placeholder="Full Name" required/>
+                <input 
+                    type="text" 
+                    className="form-control" 
+                    id="name" 
+                    name='name' 
+                    placeholder="Full Name" 
+                    // onChange={e => setField('name', e.target.value)}
+                    required/>
             </div>
             <br/>
 
             <div className="form-group">
-                <input type="email" className="form-control" id="email" name='email' placeholder="Email"/>
+                <input 
+                    type="email"
+                    className="form-control" 
+                    id="email" 
+                    name='email' 
+                    placeholder="Email" 
+                    // onChange={e => setField('email', e.target.value)}
+                    required/>
             </div>
             <br/>
 
             <div className="form-group">
-                <input type="text" className="form-control" id="phone-number" name='phone-number' placeholder="Phone Number"/>
+                <input 
+                    type="text" 
+                    className="form-control" id="phoneNumber" name='phoneNumber' placeholder="Phone Number" 
+                    // onChange={e => setField('phoneNumber', e.target.value)}
+                    required/>
             </div>
             <br/>
 
             <div class="form-group">
-                <textarea class="form-control" id="message" name='message' rows="2" placeholder='Enter message' required></textarea>
+                <textarea 
+                className="form-control" 
+                id="message" 
+                name='message' 
+                rows="2" 
+                placeholder='Enter message' 
+                // onChange={e => setField('message', e.target.value)}
+                required></textarea>
             </div>
             <br/>
 
