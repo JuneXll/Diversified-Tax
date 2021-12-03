09e4eeed6e4845eddfaa1cfb6fd1b437bff3ff9a
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-12-03 16:20:17 -0500

    Created form component

--- src/components/pages/Contact-us.js
+++ src/components/pages/Contact-us.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Container, Button  } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Instagram from '../widgets/Instagram';
+import ContactForm from '../widgets/ContactForm';
 
 //Header stylings 
 const header = {
@@ -32,13 +33,6 @@ const yellowDiv = {
     borderRadius: 5,
 }
 
-const blueButton = {
-    backgroundColor:'#003054',
-    fontFamily: 'Bebas Neue, cursive',
-    letterSpacing: '4px',
-    border: 'none'
-}
-
 const Contact = () => {
     
     return (
@@ -52,47 +46,9 @@ const Contact = () => {
                     {/* Contact us form */}
                     <Container className="p-5 d-flex justify-content-between row">
                         <Container className='p-3 mb-5 col-md-5 col-xs-12' style={yellowDiv}  id='contactForm'>
-                            <form
-                                name='contact v1' 
-                                method='post'
-                                onSubmit="submit"
-                                netlify
-                            >
-                                <input type="hidden" name="form-name" value="contact v1"/>
+                            {/* Contact us form component */}
+                            <ContactForm/>
 
-                                <input 
-                                    type="text" 
-                                    placeholder="Full Name" name="full-name" 
-                                    className="form-control"
-                                    required/>
-                                <br/>
-                                <input 
-                                    type="email" 
-                                    placeholder="Email" 
-                                    name="email" 
-                                    className="form-control"
-                                    required/>
-                                <br/>
-                                <input 
-                                    type="text" 
-                                    placeholder="Phone Number" 
-                                    name="phone-number"
-                                    className="form-control"
-                                    required/>
-                                <br/>
-                            
-                                <select 
-                                    aria-label="Personal or Business Dropdown" 
-                                    className='form-select'
-                                    name="dropdown">
-                                        <option disable>Personal or Business</option>
-                                        <option value="personal">Personal</option>
-                                        <option value="business">Business</option>
-                                        <option value="both">Personal and Business</option>
-                                </select>
-                                <br/>
-                                <Button type="submit" className='float-end' style={blueButton}>Submit</Button>
-                            </form>
                         </Container>
                         {/* Contact info */}
                         <Container className='flex-column col-md-5 col-xs-12 align-content-center p-3 mb-5' style={yellowDiv}>
---
+++ src/components/widgets/ContactForm.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const blueButton = {
+    backgroundColor:'#003054',
+    fontFamily: 'Bebas Neue, cursive',
+    letterSpacing: '4px',
+    border: 'none',
+    borderRadius: '5px'
+}
+
+
+const ContactForm = () => {
+    return(
+        <form name="contact" method="post" className='mt-2'>
+            <input type="hidden" name="form-name" value="contact" />
+      
+            <div className="form-group">
+                <input type="text" className="form-control" id="name" name='name' placeholder="Full Name" required/>
+            </div>
+            <br/>
+            <div className="form-group">
+                <input type="email" className="form-control" id="email" name='email' placeholder="Email"/>
+            </div>
+            <br/>
+            <div className="form-group">
+                <input type="number" className="form-control" id="phone-number" name='phone-number' placeholder="Phone Number"/>
+            </div>
+            <br/>
+            {/* <div className="form-group">
+                <select 
+                    aria-label="Personal or Business Dropdown" 
+                    className='form-select'
+                    name="dropdown">
+                    <option disable>Personal or Business</option>
+                    <option value="personal">Personal</option>
+                    <option value="business">Business</option>
+                    <option value="both">Personal and Business</option>
+                </select>
+            </div>
+            <br/> */}
+            <button type="submit" className='text-white float-end p-2' style={blueButton}>Submit</button>
+        </form>
+    )
+}
+
+export default ContactForm;
