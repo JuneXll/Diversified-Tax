6ece96b8316254d5d1a822ac090afa6446030711
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-12-10 12:54:29 -0500

    Completed loading animation on loading screen

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+//Language switch imports
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
@@ -7,6 +8,8 @@ import HttpApi from 'i18next-http-backend';
 
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Animated} from "react-animated-css";
+
 
 i18n
   .use(initReactI18next)//passes i18n down to react-i18next
@@ -31,7 +34,7 @@ i18n
     backgroundColor: "#003054",
     marginRight: 10,
     marginTop: '3em',
-    animation: "ball .5 ease infinite alternate"
+    animation: ".5 ease infinite alternate"
   }
 
   const text = {
@@ -41,9 +44,17 @@ i18n
   const loadingMarkup = (
     <div className="loading d-column w-100 text-center">
       <div className="balls d-flex justify-content-center">
-        <div className="ball ball1 display-1" style={ballStyle}></div>
-        <div className="ball2 display-1" style={ballStyle}></div>
-        <div className="ball2 display-1" style={ballStyle}></div>
+        <Animated animationIn="bounce" animationOut="pulse" animationInDuration={1000} animationOutDuration={1000} isVisible={true}>
+          <div className="ball1 display-1" style={ballStyle}></div>
+        </Animated>
+
+        <Animated animationIn="bounce" animationOut="pulse" animationInDuration={1400} animationOutDuration={1400} isVisible={true}>
+          <div className="ball2 display-1" style={ballStyle}></div>
+        </Animated>
+
+        <Animated animationIn="bounce" animationOut="pulse" animationInDuration={1800} animationOutDuration={1800} isVisible={true}>
+          <div className="ball3 display-1" style={ballStyle}></div>
+        </Animated>
       </div>
       <span className="display-3" style={text}>Loading</span>
     </div>
@@ -55,10 +66,3 @@ ReactDOM.render(
     </Suspense>,
   document.getElementById('root')
 );
-
-{/* <div className='py-4 text-center'>
-      <h2 className='display-2'>
-        Loading...
-        <span className="visually-hidden">Loading...</span>
-      </h2>
-    </div> */}
