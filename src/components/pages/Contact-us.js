f2bc31d920a396280f85a43a80eabf033c9f0ad8
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-12-20 14:47:10 -0500

    Removal was unsuccessful added code back

--- src/components/pages/Contact-us.js
+++ src/components/pages/Contact-us.js
@@ -35,12 +35,10 @@ const yellowDiv = {
 
 const showIG = () => {
     const igFeed = document.getElementById("ig-feed");
-    const wrapper = document.getElementByClass("poweredbywrapper");
 
     if(window.location.href === 'https://diversified-tax.netlify.app/contact-us') {
         if(igFeed.style.display === 'none') {
             igFeed.style.display = 'block';
-            wrapper.style.display = 'none';
         } else {
             igFeed.style.display = 'none';
         }
