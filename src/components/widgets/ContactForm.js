dc73012415c8c1bb428a8e6cb8dd341b64757a93
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-12-22 14:30:12 -0500

    Testing post to netlify

--- src/components/widgets/ContactForm.js
+++ src/components/widgets/ContactForm.js
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+//Validation of form
+// import { navigate } from 'gatsby'
+import { useForm } from 'react-hook-form';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from 'yup';
 import { useTranslation } from 'react-i18next';
 
 const blueButton = {
@@ -9,87 +14,120 @@ const blueButton = {
     borderRadius: '5px'
 }
 
-const ContactForm = () => {
+// const schema = yup.object().shape({
+//     name: yup.string().required(),
+//     email: yup.string().required().email(),
+//     phoneNumber: yup.number().required().min(10).max(10),
+//     message: yup.string().required().min(10)
 
+// });
+
+const encode = data => {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  }
+
+const ContactForm = () => {
+    //Set translation for page
     const { t } = useTranslation();
 
-    (function () {
-      
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.querySelectorAll('.needs-validation')
-      
-        // Loop over them and prevent submission
-        Array.prototype.slice.call(forms)
-          .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-              if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-              }
-      
-              form.classList.add('was-validated')
-            }, false)
-          })
-      })()
+     //react-hook form
+     const{  
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset
+      } = useForm({
+        // resolvers: yupResolver(schema)
+        mode: 'onChange'
+    });
 
+   const [state, setState] = useState({});
+
+   const handleChange = e => {
+    setState({...state, [e.target.name]: e.target.value})
+    };
+    
+    // submit
+    const onSubmit = (data, e) => {
+        e.preventDefault();
+        // const form = e.target;
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: encode({ 
+                "form-name": "contact",
+                ...data
+            })
+        })
+          .then(response=>{
+              reset();
+              console.log(response);
+          })
+          .catch(error => console.log(error));
+      };
+    // const onSubmit = (data) => console.log(data);
+    // console.log(errors)
 
     return(
-        <form name="contact v1" method="post" className='mt-2 needs-validation' noValidate>
+        <form 
+            name="contact v1" 
+            method="POST" 
+            className='mt-2' 
+            onSubmit={handleSubmit(onSubmit)}>
             <input type="hidden" name="form-name" value="contact" />
       
-            <div className="form-group has-validation">
+            <div className="form-group">
                 <input 
                     type="text" 
                     className="form-control" 
                     id="name" 
                     name='name' 
                     placeholder={t('contact_form_name')}
-                    required/>
-                    <div class="invalid-feedback">
-                        Please enter full name.
-                    </div>
+                    onChange={handleChange}
+                    {...register('name')}
+                    />
+                    {errors.name && <p>Name is required</p>}
             </div>
             <br/>
 
-            <div className="form-group has-validation">
+            <div className="form-group">
                 <input 
                     type="email"
                     className="form-control" 
                     id="email" 
                     name='email' 
                     placeholder={t('contact_form_email')}
-                    required/>
-                    <div class="invalid-feedback">
-                        Please enter valid email.
-                    </div>
+                    onChange={handleChange}
+                    {...register('email')}
+                    />
             </div>
             <br/>
 
-            <div className="form-group has-validation">
+            <div className="form-group">
                 <input 
                     type="text" 
                     className="form-control" 
                     id="phoneNumber" 
                     name='phoneNumber' 
                     placeholder={t('contact_form_phone')}
-                    required/>
-                    <div class="invalid-feedback">
-                        Please enter phone number.
-                    </div>
+                    onChange={handleChange}
+                    {...register('phoneNumber')}
+                    />
             </div>
             <br/>
 
-            <div class="form-group has-validation">
+            <div class="form-group">
                 <textarea 
                     className="form-control" 
                     id="message" 
                     name='message' 
                     rows="2" 
                     placeholder={t('contact_form_message')} 
-                    required></textarea>
-                    <div class="invalid-feedback">
-                        Please write a message.
-                    </div>
+                    onChange={handleChange}
+                    {...register('message')}
+                    ></textarea>
             </div>
             <br/>
 
