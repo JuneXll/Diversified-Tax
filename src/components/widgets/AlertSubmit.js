2aa5a7102815b508fe2ea2592de9bf3883ca7b09
JuneXll <71652307+JuneXll@users.noreply.github.com>
2023-01-15 13:54:33 -0500

    updated

--- src/components/widgets/AlertSubmit.js
+++ src/components/widgets/AlertSubmit.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Alert,Button } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
+//Styles for banner
 const openAlert = {
     backgroundColor: '#E1AD00',
     borderColor: '#E1AD00',
@@ -24,27 +26,32 @@ const alertStyles = {
     boxShadow: '5px 5px 5px #ccc'
 }
 
+const taxCheck = "https://drive.google.com/file/d/1jT7Yf0uZjlgECPA82-9UUjUlZxYxi7Sn/view"
+
 const AlertSubmit = () => {
     const [show, setShow] = useState(false);
+
+    //Sets translations for the page
+    const { t } = useTranslation();
           
     return (
         <>
             <Alert show={show} variant="warning" style={alertStyles}>
-                <Alert.Heading><strong>Forgot Something?</strong></Alert.Heading>
+                <Alert.Heading><strong>{t('alert_heading')}</strong></Alert.Heading>
                 <p>
-                    <strong>Don't sweat it! Click <Alert.Link href='/extra-forms'>here</Alert.Link> to submit your extra documents.</strong>
+                    <strong>{t('alert_click')}<Alert.Link href={taxCheck} target='_blank'> {t('alert_here')}</Alert.Link> {t('alert_download_cl')}</strong>
                 </p>
                 <hr />
                 <div className="d-flex justify-content-end">
                     <Button style={allDone} onClick={() => setShow(false)} >
-                      All done!
+                    {t('alert_all_done')}
                     </Button>
                 </div>
                 </Alert>
           
                 {!show && 
                     <div className='d-grid my-2'>
-                        <Button style={openAlert} onClick={() => setShow(true)}>Forgot to submit extra documents?</Button>
+                        <Button style={openAlert} onClick={() => setShow(true)}>{t('alert_not_sure')}</Button>
                     </div>
                 }
               </>
